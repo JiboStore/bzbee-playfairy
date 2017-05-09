12d5d3d461d463809ae533b6824def9fc1d7f280
Hakim Hauston <hakim.hauston@gmail.com>
2017-05-09 11:38:04 +0800

    [PlayFairy] ipa downloadable from desktop browser

--- app/com/playfairy/controllers/DownloadController.java
+++ app/com/playfairy/controllers/DownloadController.java
@@ -1,5 +1,13 @@
 package com.playfairy.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import org.apache.commons.io.FilenameUtils;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -17,10 +25,26 @@ public class DownloadController extends Controller {
     }
     
     public Result ipa() {
-  	return ok(com.playfairy.controllers.views.html.download.index.render());
+    	List<String> ipaNames = new ArrayList<String>();
+    	try {
+    		File ipaDir = new File("public/ipa/uploads/");
+    		if ( ipaDir.exists() && ipaDir.isDirectory() ) {
+    			File[] ipaFiles = ipaDir.listFiles();
+    			if ( ipaFiles != null ) {
+    				for ( int i = 0; i < ipaFiles.length; i++ ) {
+    					if ( "ipa".equals(FilenameUtils.getExtension(ipaFiles[i].getName())) ) {
+    						ipaNames.add(FilenameUtils.getBaseName(ipaFiles[i].getName()));
+    					}
+    				}
+    			}
+    		}
+    	} catch ( Exception ioe ) {
+    		return ok(com.playfairy.controllers.views.html.download.ipa.render(ipaNames));
+    	}
+    	return ok(com.playfairy.controllers.views.html.download.ipa.render(ipaNames));
   }
     
-    public Result plist(int id) {
+    public Result plist(String id) {
     	return ok(com.playfairy.controllers.views.html.download.plist.render(id));
     }
 
