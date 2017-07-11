a0804b731c62d97519190107a0e3752acd86bb8c
Hakim Hauston <hakim.hauston@gmail.com>
2017-07-11 14:39:34 +0800

    [PlayFairy] create project controller and view

---
+++ app/com/playfairy/controllers/ProjectController.scala
@@ -0,0 +1,112 @@
+package com.playfairy.controllers
+
+import play.api.mvc._
+import javax.inject.Inject
+import play.modules.reactivemongo.ReactiveMongoApi
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.ReactiveMongoComponents
+import play.api.libs.json._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import reactivemongo.bson._
+import reactivemongo.api.commands.WriteResult
+import com.playfairy.models._
+import play.Logger
+import scala.concurrent.Future
+import scala.util.Failure
+import scala.util.Success
+import play.api.data._
+import play.api.data.Forms._
+
+class ProjectController @Inject() (reactiveMongoApi: ReactiveMongoApi) 
+  extends Controller with MongoController with ReactiveMongoComponents {
+  
+  def reactiveMongoApi() : ReactiveMongoApi = {
+    return reactiveMongoApi;
+  }
+  
+//  def booksRepo = new BooksRepoImpl(reactiveMongoApi)
+//  def personRepo = new PersonRepoImpl(reactiveMongoApi)
+  def projectRepo = new ProjectRepoImpl(reactiveMongoApi)
+  
+  def index() : Action[AnyContent] = Action.async {
+    Future{
+      Ok(com.playfairy.controllers.views.html.project.index())
+    }
+  }
+  
+  case class CreateProjectFormData(projectName: String)
+  
+  def create() : Action[AnyContent] = Action.async { implicit request =>
+    val formData = Form(
+        mapping(
+            "projectName" -> nonEmptyText
+        )(CreateProjectFormData.apply)(CreateProjectFormData.unapply)
+    )
+    formData.bindFromRequest.fold(
+        hasErrors => {
+          Future {
+            BadRequest("form binding error")
+          }
+        },
+        success => {
+          val futureResult = projectRepo.createProject(success.projectName)
+          futureResult.map( writeResult => {
+            if ( writeResult.ok ) {
+              Ok("project created")
+            } else {
+              Ok("unable to create project")
+            }
+          })
+        }
+    )
+  }
+  
+//  case class SignupFormData(username: String, password:String)
+//  
+//  def createPerson() : Action[AnyContent] = Action.async { implicit request =>
+//    val signupForm = Form(
+//        mapping(
+//            "username" -> nonEmptyText,
+//            "password" -> nonEmptyText
+//        )(SignupFormData.apply)(SignupFormData.unapply)
+//    )
+//    signupForm.bindFromRequest.fold(
+//        hasErrors => {
+//          Future {
+//            BadRequest("form binding error")
+//          }
+//        },
+//        success => {
+//          val res = personRepo.createPerson( success.username, success.password )
+//          res.map( r => {
+//            if ( r.ok ) {
+//              Ok("inserted")
+//            } else {
+//              Ok("failed to insert")
+//            }
+//          })
+//        }
+//    )
+//  }
+  
+//  def findByName(name: String) = Action.async {
+//    Logger.debug("findByName: " + name);
+//    var future = booksRepo.findByName(name)
+//    future.map( listBooks => {
+//      Ok( Json.toJson(listBooks) )
+//    });
+//  }
+  
+//  def updateByName(name: String) = Action.async {
+//    var newRole = List("one", "two", "three")
+//    var future = booksRepo.updateByName("hello", newRole)
+//    future.map(writeResult => {
+//      if ( writeResult.ok ) {
+//        Ok("ok")
+//      } else {
+//        Ok("problem")
+//      }
+//    });
+//  }
+  
+}
